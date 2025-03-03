4aee3a9740e6d23e0d15b8b092f167b6a2744609
Hamdhan Ahamed <ahamedhamthan7777@gmail.com>
2025-03-03 16:24:57 +0530

    feature:hero section dev

--- vepay-landing-page/src/App.jsx
+++ vepay-landing-page/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Nav from "./components/Nav.jsx";
+import Hero from "./components/Hero.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Nav></Nav>
+        <Hero></Hero>
     </>
   )
 }
